1c4e69d81c67e1aadba73cef56592d913c9c2957
tkottke90 <altotom90@gmail.com>
2017-07-13 16:51:54 -0500

    Node.js Server Built

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 "use strict"
 
+var express = require('express');
+var app = express();
+
+
+app.get('/', function(req, res){
+    console.log("Hello World!");
+    res.send("Hello World!");
+});
+
+var server = app.listen(8080, function(){
+    var now = new Date().toUTCString();
+    var port = server.address().port;
+
+    console.log(`${now} - [Server] - Server started on port: ${port}`)
+});
+
+
+/**
+ * Status Message Reference:
+ * 
+ * Server Message:
+ *      `${new Date().toUTCString} - [Server] -
+ * 
+ * Database Message: 
+ *      `${new Date().toUTCString} - [Database] -
+ * 
+ * Error Message:
+ *      Minor -> `${new Date().toUTCString} - [Error] - `
+ *      Sever -> `${new Date().toUTCString} - [ERROR] - `
+ *      Crash -> `${new Date().toUTCString} - [Crash] -
+ */
