b14ddaf188aa6dfd429f5e1ee17dad28d397315c
tkottke90 <altotom90@gmail.com>
2017-07-14 11:52:35 -0500

    Added References to Redis DB

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 "use strict"
 
+// Import Modules
 var express = require('express');
+var redis = require('redis');
+
+// Module Variables
 var app = express();
+var client = redis.createClient();
+
+// Redis Methods
+client.monitor(function(err, res){
+    console.log(`${new Date().toUTCString()} - [Database] - Entering Monitoring Mode`);
+});
+
+client.on('connect', function(){
+    console.log(`${new Date().toUTCString()} - [Database] - Redis Client Connected `);
+    
+    client.INFO("stats", function(err, data){
+        console.log(data);
+    });   
+    
+    console.log(`${new Date().toUTCString()} - [Database] - [SET] - Post Request`)
+    client.set("framework", "NodeJS", function(err, reply){
+        err ? console.log(`${new Date().toUTCString()} - [Database] - [SET] - Error: ${err} `) : console.log(`${new Date().toUTCString()} - [Database] - [SET] - Successful Post `);
+    });
+
+});
+
+client.on('monitor', function(time, args, raw_reply){
+    console.log(`${new Date().toUTCString()} - [Database-Monitor] - ${args}`);
+});
 
 
+// App Methods
 app.get('/', function(req, res){
-    console.log("Hello World!");
+    console.log(`${new Date().toUTCString()} - [Server] - Hello World!`);
+    client.get('framework', function(err, reply){
+        console.log(`${new Date().toUTCString()} - [Database] - [GET] - ${reply}`);
+    });
     res.send("Hello World!");
 });
 
+
+// Server Listener
 var server = app.listen(8080, function(){
     var now = new Date().toUTCString();
     var port = server.address().port;
@@ -21,13 +55,18 @@ var server = app.listen(8080, function(){
  * Status Message Reference:
  * 
  * Server Message:
- *      `${new Date().toUTCString} - [Server] -
+ *      `${new Date().toUTCString()} - [Server] -`
  * 
  * Database Message: 
- *      `${new Date().toUTCString} - [Database] -
+ *      `${new Date().toUTCString()} - [Database] - `
+ *      `${new Date().toUTCString()} - [Database] - [GET] - `
+ *      `${new Date().toUTCString()} - [Database] - [SET] - `
+ *      `${new Date().toUTCString()} - [Database] - [DELETE] - `
+ * 
+ *      `${new Date().toUTCString()} - [Database-Monitor] - `
  * 
  * Error Message:
- *      Minor -> `${new Date().toUTCString} - [Error] - `
- *      Sever -> `${new Date().toUTCString} - [ERROR] - `
- *      Crash -> `${new Date().toUTCString} - [Crash] -
+ *      Minor -> `${new Date().toUTCString()} - [Error] - `
+ *      Sever -> `${new Date().toUTCString()} - [ERROR] - `
+ *      Crash -> `${new Date().toUTCString()} - [Crash] -
  */
